3b16b64e2b4f400b5c3dc2806db320a157eff39c
Squidlider <41284983+Squidlider@users.noreply.github.com>
2019-09-20 16:42:06 +0300

    Updates

---
+++ precache-manifest.f9af69f8a8bcb6354bfcb24a06d462a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e4057f0bf898edf8167",
+    "url": "/Mainosmaker/static/css/main.658f87f1.chunk.css"
+  },
+  {
+    "revision": "4e4057f0bf898edf8167",
+    "url": "/Mainosmaker/static/js/main.4e4057f0.chunk.js"
+  },
+  {
+    "revision": "a28386935be018e238d3",
+    "url": "/Mainosmaker/static/css/1.96005c93.chunk.css"
+  },
+  {
+    "revision": "a28386935be018e238d3",
+    "url": "/Mainosmaker/static/js/1.a2838693.chunk.js"
+  },
+  {
+    "revision": "a617f433c793e6e2c8eb",
+    "url": "/Mainosmaker/static/js/runtime~main.a617f433.js"
+  },
+  {
+    "revision": "8fc622c3a2e2d992ec059cca61e3dfc0",
+    "url": "/Mainosmaker/static/media/impact.8fc622c3.ttf"
+  },
+  {
+    "revision": "b5d7eb6241bb7e721ad1aec780222f5e",
+    "url": "/Mainosmaker/index.html"
+  }
+];
